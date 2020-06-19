8e91066f739df6069d90597a51b73c727423ac66
Cameron Ibrahim <dev@cameronibrahim.com>
2020-06-19 11:01:28 -0700

    Added makefile, added type annotation to get program to stop complaining
    Added comments to fortran code

--- benchmark/runc.sh
+++
@@ -1,2 +0,0 @@
-ifort  -g -I$MKLROOT/include  -m64 ./curve.f90  -Wl,--start-group $MKLROOT/lib/intel64/libmkl_gf_lp64.a $MKLROOT/lib/intel64/libmkl_intel_thread.a $MKLROOT/lib/intel64/libmkl_core.a -Wl,--end-group   -L$MKLROOT/../compilers_and_libraries_2020.1.217/linux/compiler/lib/intel64_lin/ -liomp5 -lpthread  -ldl -DUSE_MM_MALLOC -DSTANDALONE -DOPENQU_HAVE_MPI -DMKL_ILP64
-
