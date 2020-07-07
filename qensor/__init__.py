1fd4997f868eda02c7ed8e0044f138b8ced01ad9
Danil Lykov <lkv97dn@gmail.com>
2020-07-07 14:29:26 -0500

    add option to simulate without lightcones

--- qensor/__init__.py
+++ qensor/__init__.py
@@ -24,6 +24,56 @@ class QtreeQAOAComposer(QAOAComposer, QtreeCreator):
         self.circuit = self.circuit + list(reversed(conjugate))
         return self.circuit
 
+def QAOA_energy_no_lightcones(G, gamma, beta, profile=False):
+    total_E = 0
+    if profile:
+        backend = PerfNumpyBackend(num_lines=5)
+    else:
+        backend = NumpyBackend()
+    sim = QtreeSimulator(bucket_backend=backend)
+
+    for edge in G.edges():
+        i,j = edge
+        graph = G
+
+        composer = QtreeQAOAComposer(
+            graph=graph, gamma=gamma, beta=beta)
+
+        composer.energy_expectation(i,j)
+        if hasattr(sim, 'peo'):
+            peo=sim.peo
+        else:
+            pass
+        peo=None
+
+        result = sim.simulate_state(composer.circuit, peo=peo)
+        E = result.data
+        if profile:
+            print(backend.gen_report())
+            print(E)
+        total_E += E
+
+    E = total_E
+    #print(result.data)
+    #print(composer.circuit)
+    if np.imag(E)>1e-6:
+        print(f"Warning: Energy result imaginary part was: {np.imag(E)}")
+
+    """
+    C = sum(CC)
+    2*CC = 1 - ZZ
+    2*C = sum(1-CC)
+    2*C = Ed - sum(CC)
+    C = (Ed - E)/2
+    """
+    E = np.real(E)
+
+    Ed = G.number_of_edges()
+    C = (Ed - E)/2
+
+    return C
+
+
 def QAOA_energy(G, gamma, beta, profile=False):
     total_E = 0
     if profile:
