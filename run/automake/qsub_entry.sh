034c6338e37f3a3bf2707c181550367b69827e73
Danil Lykov <lkv97dn@gmail.com>
2020-08-01 16:48:58 -0500

    where is the file...

--- run/automake/qsub_entry.sh
+++ run/automake/qsub_entry.sh
@@ -2,7 +2,13 @@
 echo "Dear God, I am $(hostname)!"
 lscpu
 python -c "import matplotlib.pyplot as plt; plt.plot(range(55)); plt.savefig('results/image.png')"
-ls
+sleep 1
 ls results
+echo 'ls'
+ls
+echo 'pwd'
 pwd
+echo 'test'>test.txt
+echo 'ls'
+ls
 python -c "print('Hello from python')"
