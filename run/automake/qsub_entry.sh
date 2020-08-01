f83580e4ed241467f6eb856e48a033223147af2b
Danil Lykov <lkv97dn@gmail.com>
2020-08-01 16:38:57 -0500

    check where we run in cobalt

--- run/automake/qsub_entry.sh
+++ run/automake/qsub_entry.sh
@@ -2,3 +2,6 @@
 echo "Dear God, I am $(hostname)!"
 lscpu
 python -c "import matplotlib.pyplot as plt; plt.plot(range(55)); plt.savefig('results/image.png')"
+ls
+pwd
+python -c "print('Hello from python')"
