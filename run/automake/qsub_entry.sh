90164f1592dbff9c443c5579cd0aa9676a401ecd
Danil Lykov <lkv97dn@gmail.com>
2020-08-01 16:53:09 -0500

    write test file from python

--- run/automake/qsub_entry.sh
+++ run/automake/qsub_entry.sh
@@ -2,6 +2,7 @@
 echo "Dear God, I am $(hostname)!"
 lscpu
 python -c "import matplotlib.pyplot as plt; plt.plot(range(55)); plt.savefig('results/image.png')"
+python -c "f = open('w+', 'results/test.txt'); f.write('fff'); f.close()"
 sleep 1
 ls results
 echo 'ls'
