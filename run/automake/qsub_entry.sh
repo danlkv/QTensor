edf68124920ded64a8c74785e4d18fab9366ab5b
Danil Lykov <lkv97dn@gmail.com>
2020-08-01 17:01:04 -0500

    output envs

--- run/automake/qsub_entry.sh
+++ run/automake/qsub_entry.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo "Dear God, I am $(hostname)!"
 lscpu
+echo $PYTHONPATH
+echo $PATH
+echo $SHELL
 python -c "import matplotlib.pyplot as plt; plt.plot(range(55)); plt.savefig('results/image.png')"
-python -c "f = open('w+', 'results/test.txt'); f.write('fff'); f.close()"
-sleep 1
-ls results
-echo 'ls'
-ls
 echo 'pwd'
 pwd
 echo 'test'>test.txt
--- run/automake/run.sh
+++ run/automake/run.sh
@@ -5,6 +5,7 @@ echo "Starting session to queue $queue..."
 job_id=$(qsub -t300 -n1 -q $queue --cwd=$(pwd) qsub_entry.sh)
 echo "Qsub Job: $job_id"
 logfile=$job_id.output
+errfile=$job_id.error
 
 sleep 3
 tail --retry -f $logfile &
@@ -12,4 +13,6 @@ tail_pid=$!
 
 ./job_wait.sh $job_id &
 wait $!
+echo "Errors:"
+cat $errfile
 kill $tail_pid
