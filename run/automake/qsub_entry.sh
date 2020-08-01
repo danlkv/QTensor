ba190e8569b8a6d5f67de7559d5ca6767db0ee22
Danil Lykov <lkv97dn@gmail.com>
2020-08-01 16:44:17 -0500

    where is the image...

--- run/automake/qsub_entry.sh
+++ run/automake/qsub_entry.sh
@@ -3,5 +3,6 @@ echo "Dear God, I am $(hostname)!"
 lscpu
 python -c "import matplotlib.pyplot as plt; plt.plot(range(55)); plt.savefig('results/image.png')"
 ls
+ls results
 pwd
 python -c "print('Hello from python')"
