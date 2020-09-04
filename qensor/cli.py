af3f2c27e55644da27d489f3952f34b77ce39b6a
Danil Lykov <lkv97dn@gmail.com>
2020-09-04 00:06:30 -0500

    add 'without' optimizer. reverse order mystery

--- qensor/cli.py
+++ qensor/cli.py
@@ -11,7 +11,7 @@ import qensor.optimisation as qop
 from qensor.FeynmanSimulator import FeynmanSimulator
 
 from qensor.optimisation.TensorNet import QtreeTensorNet
-from qensor.optimisation.Optimizer import OrderingOptimizer, TamakiOptimizer
+from qensor.optimisation.Optimizer import OrderingOptimizer, TamakiOptimizer, WithoutOptimizer
 from qensor import QtreeQAOAComposer
 
 @click.group()
@@ -85,6 +85,29 @@ def tw_heuristic(filename, tamaki_time):
 @click.option('-n','--nodes', default=10)
 @click.option('-p','--p', default=1)
 @click.option('-G','--graph-type', default='random_regular')
+def generate_qaoa_ansatz_circuit(seed, degree, nodes, p, graph_type):
+    np.random.seed(seed)
+    if graph_type=='random_regular':
+        G = nx.random_regular_graph(degree, nodes)
+    elif graph_type=='erdos_renyi':
+        G = nx.erdos_renyi_graph(nodes, degree/(nodes-1))
+    elif graph_type=='erdos_renyi_core':
+        G = nx.erdos_renyi_graph(nodes, degree/(nodes-1))
+        G = nx.algorithms.core.k_core(G, k=degree)
+    else:
+        raise Exception('Unsupported graph type')
+    gamma, beta = [0]*p, [0]*p
+    composer = QtreeQAOAComposer(G, beta=beta, gamma=gamma)
+    composer.ansatz_state()
+    txt = qtree.operators.circuit_to_text([composer.circuit], nodes)
+    print(txt)
+
+@cli.command()
+@click.option('-s','--seed', default=42)
+@click.option('-d','--degree', default=3)
+@click.option('-n','--nodes', default=10)
+@click.option('-p','--p', default=1)
+@click.option('-G','--graph-type', default='random_regular')
 @click.option('-E','--edge-index', default=0)
 def generate_qaoa_energy_circuit(seed, degree, nodes, p, graph_type, edge_index):
     np.random.seed(seed)
@@ -94,9 +117,7 @@ def generate_qaoa_energy_circuit(seed, degree, nodes, p, graph_type, edge_index)
         G = nx.erdos_renyi_graph(nodes, degree/(nodes-1))
     elif graph_type=='erdos_renyi_core':
         G = nx.erdos_renyi_graph(nodes, degree/(nodes-1))
-        print('degrees', list(G.degree))
         G = nx.algorithms.core.k_core(G, k=degree)
-        print('nodes', G.number_of_nodes())
     else:
         raise Exception('Unsupported graph type')
     gamma, beta = [0]*p, [0]*p
@@ -137,6 +158,8 @@ def qaoa_energy_tw(nodes, seed, degree, p, graph_type, max_time, ordering_algo):
             opt = OrderingOptimizer()
         elif ordering_algo=='tamaki':
             opt = TamakiOptimizer(wait_time=45)
+        elif ordering_algo=='without':
+            opt = WithoutOptimizer()
         else:
             raise ValueError("Ordering algorithm not supported")
         peo, tn = opt.optimize(tn)
