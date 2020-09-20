5a928006c6d597f8dcb296ce50d723ab468bf969
Danil Lykov <lkv97dn@gmail.com>
2020-09-19 22:02:11 -0500

    refactor to builder pattern

--- qtensor/CircuitComposer.py
+++ qtensor/CircuitComposer.py
@@ -3,10 +3,44 @@ from qtensor.utils import get_edge_subgraph
 import networkx as nx
 from .OpFactory import CircuitBuilder
 
-class CircuitComposer(CircuitBuilder):
+class CircuitComposer():
+    """ Director for CircuitBuilder, but with a special way to get the builder"""
+    Bulider = CircuitBuilder
     def __init__(self, *args, **params):
-        super().__init__(*args, **params)
         self.params = params
+        self.builder = self._get_builder()
+        self.n_qubits = self.builder.n_qubits
+
+    #-- Setting up the builder
+    def _get_builder_class(self):
+        raise NotImplementedError
+
+    def _get_builder(self):
+        return self._get_builder_class()()
+
+
+    #-- Mocking some of bulider behaviour
+    @property
+    def operators(self):
+        return self.builder.operators
+
+    @property
+    def circuit(self):
+        return self.builder.circuit
+    @circuit.setter
+    def circuit(self, circuit):
+        self.builder.circuit = circuit
+
+    @property
+    def qubits(self):
+        return self.builder.qubits
+    @qubits.setter
+    def qubits(self, qubits):
+        self.builder.qubits = qubits
+
+    def apply_gate(self, gate, *qubits, **params):
+        self.builder.apply_gate(gate, *qubits, **params)
+    #--
 
     def layer_of_Hadamards(self):
         for q in self.qubits:
@@ -14,11 +48,15 @@ class CircuitComposer(CircuitBuilder):
 
 
 class QAOAComposer(CircuitComposer):
+    """ Abstract base class for QAOA Director """
     def __init__(self, graph, *args, **kwargs):
-        n_qubits = graph.number_of_nodes()
-        super().__init__(n_qubits, *args, **kwargs)
+        self.n_qubits = graph.number_of_nodes()
+        super().__init__(*args, **kwargs)
 
         self.graph = graph
+
+    def _get_builder(self):
+        return self._get_builder_class()(self.n_qubits)
 
     @classmethod
     def _get_of_my_type(cls, *args, **kwargs):
@@ -36,6 +74,23 @@ class QAOAComposer(CircuitComposer):
 
         self.circuit = self.circuit + list(reversed(conjugate))
         return self.circuit
+
+    def energy_expectation_lightcone(self, edge):
+        G = self.graph
+        gamma, beta = self.params['gamma'], self.params['beta']
+        i,j = edge
+        # TODO: take only a neighbourhood part of the graph
+        graph = get_edge_subgraph(G, edge, len(gamma))
+        log.debug('Subgraph nodes: {}, edges: {}', graph.number_of_nodes(), graph.number_of_edges())
+        graph = get_edge_subgraph(G, edge, len(gamma))
+        mapping = {v:i for i, v in enumerate(graph.nodes())}
+        graph = nx.relabel_nodes(graph, mapping, copy=True)
+
+        i,j = mapping[i], mapping[j]
+        composer = self._get_of_my_type(graph, beta=beta, gamma=gamma)
+        composer.energy_expectation(i,j)
+        self.circuit = composer.circuit
+        # return composer
 
 
     def x_term(self, u, beta):
--- qtensor/__init__.py
+++ qtensor/__init__.py
@@ -15,43 +15,17 @@ from qtensor.QAOASimulator import QAOAQtreeSimulator
 from qtensor.FeynmanSimulator import FeynmanSimulator
 from qtensor.ProcessingFrameworks import PerfNumpyBackend, NumpyBackend
 
-class CirqQAOAComposer(QAOAComposer, CirqBuilder):
-    pass
+class CirqQAOAComposer(QAOAComposer):
+    def _get_builder_class(self):
+        return CirqBuilder
 
-class QiskitQAOAComposer(QAOAComposer, QiskitBuilder):
-    pass
+class QiskitQAOAComposer(QAOAComposer):
+    def _get_builder_class(self):
+        return QiskitBuilder
 
-class QtreeQAOAComposer(QAOAComposer, QtreeBuilder):
-    def energy_expectation(self, i, j):
-        G = self.graph
-        self.ansatz_state()
-        self.energy_edge(i, j)
-
-        beta, gamma = self.params['beta'], self.params['gamma']
-        conjugate = QtreeQAOAComposer(G, beta=beta, gamma=gamma)
-        conjugate.ansatz_state()
-        conjugate = [g.dagger_me() for g in conjugate.circuit]
-
-        self.circuit = self.circuit + list(reversed(conjugate))
-        return self.circuit
-
-    def energy_expectation_lightcone(self, edge):
-        G = self.graph
-        gamma, beta = self.params['gamma'], self.params['beta']
-        i,j = edge
-        # TODO: take only a neighbourhood part of the graph
-        graph = get_edge_subgraph(G, edge, len(gamma))
-        log.debug('Subgraph nodes: {}, edges: {}', graph.number_of_nodes(), graph.number_of_edges())
-        graph = get_edge_subgraph(G, edge, len(gamma))
-        mapping = {v:i for i, v in enumerate(graph.nodes())}
-        graph = nx.relabel_nodes(graph, mapping, copy=True)
-
-        i,j = mapping[i], mapping[j]
-        composer = QtreeQAOAComposer(graph, beta=beta, gamma=gamma)
-        composer.energy_expectation(i,j)
-        self.circuit = composer.circuit
-        # return composer
-
+class QtreeQAOAComposer(QAOAComposer):
+    def _get_builder_class(self):
+        return QtreeBuilder
 
 def QAOA_energy(G, gamma, beta, n_processes=0):
     sim = QAOAQtreeSimulator(QtreeQAOAComposer)
