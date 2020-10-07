30a9eb8995cdfe667824f3133039398aadebdd31
Danil <mail@example.com>
2020-10-07 09:18:53 +0000

    fix matrix_output script

--- docs/spec/generate/generate.sh
+++ docs/spec/generate/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-command='python -m qensor.cli qaoa-energy-tw --nodes 100 --p $0 --degree $1 --graph-type erdos_renyi'
-echo '{"p":[1,2], "d":[3,4]}' | ./matrix_output.py "$command"
+command='python -m qtensor.cli qaoa-energy-tw --nodes 1000 --p $0 --degree $1 --graph-type random_regular --max-time 500'
+echo '{"p":[2,3,4,5], "d":[3,4]}' | ./matrix_output.py "$command"
