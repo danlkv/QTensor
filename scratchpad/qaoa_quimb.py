befa4040a6d2a13a426e2edd66c2c2e3708974c9
Danil <mail@example.com>
2020-11-07 14:55:50 +0000

    minor fixes in qaao_quimb, added prof data

--- scratchpad/qaoa_quimb.py
+++ scratchpad/qaoa_quimb.py
@@ -13,15 +13,18 @@ from tqdm import tqdm
 SEED = 100
 
 def edge_simulate(args):
-    G, p, kwargs, edge = args
+    circ, kwargs, edge = args
 
-    terms = {(i, j):1 for i, j in G.edges}
-    gammas, betas = [0.1]*p, [.2]*p
-    circ = qtn.circ_qaoa(terms, p, gammas, betas)
     ZZ = qu.pauli('Z') & qu.pauli('Z')
     optimizer = ctg.HyperOptimizer(
         parallel=False,
         max_repeats=10000,
+        max_time=kwargs.get('optimizer_time', 1)
+    )
+    optimizer = ctg.UniformOptimizer(
+        parallel=False,
+        methods=['greedy'],
+        max_repeats=1_000_000,
         max_time=kwargs.get('optimizer_time', 1)
     )
     #return circ.local_expectation(ZZ, edge, optimize=optimizer)
@@ -29,23 +32,25 @@ def edge_simulate(args):
 
 def simulate_one_parallel(N, p, n_processes=28, **kwargs):
     G = nx.random_regular_graph(3, N, seed=SEED)
-    args = list(zip(repeat(G), repeat(p), repeat(kwargs), G.edges))
+    terms = {(i, j):1 for i, j in G.edges}
+    gammas, betas = [0.1]*p, [.2]*p
+    circ = qtn.circ_qaoa(terms, p, gammas, betas)
+    args = list(zip(repeat(circ), repeat(kwargs), G.edges))
 
     with Pool(processes=n_processes) as pool:
         contributions = list(tqdm(pool.imap(edge_simulate, args), total=len(args)))
     return sum(contributions)
 
-def simulate_one(N, p):
+def simulate_one(N, p, **kwargs):
     G = nx.random_regular_graph(3, N, seed=SEED)
 
     terms = {(i, j):1 for i, j in G.edges}
     gammas, betas = [0.1]*p, [.2]*p
     circ = qtn.circ_qaoa(terms, p, gammas, betas)
-    ZZ = qu.pauli('Z') & qu.pauli('Z')
 
     contributions = []
     for edge in tqdm(G.edges):
-        contributions.append(circ.local_expectation(ZZ, edge))
+        contributions.append(edge_simulate((circ, kwargs, edge)))
     return sum(contributions)
 
 @click.command()
@@ -61,8 +66,10 @@ def bench_quimb(nodes, p, n_processes, seed=10, optimizer_time=1):
     np.random.seed(seed)
     N = nodes
     start = time.time()
-    #E = simulate_one(N, p)
-    E = simulate_one_parallel(N, p, n_processes=n_processes, optimizer_time=optimizer_time)
+    if n_processes==1:
+        E = simulate_one(N, p, optimizer_time=optimizer_time)
+    else:
+        E = simulate_one_parallel(N, p, n_processes=n_processes, optimizer_time=optimizer_time)
     end = time.time()
     print(f'Time for {N=}, {p=}; {E=}: {end-start}')
 
