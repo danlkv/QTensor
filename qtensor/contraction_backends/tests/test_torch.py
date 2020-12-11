281e3498ca9bfdc0e3573b1876f85dca007e92d5
Danil Lykov <lkv97dn@gmail.com>
2020-12-11 01:37:05 -0600

    torch getting sliced buckets works

--- qtensor/contraction_backends/tests/test_torch.py
+++ qtensor/contraction_backends/tests/test_torch.py
@@ -1,6 +1,42 @@
 import qtensor
+import numpy as np
 from qtensor.contraction_backends import TorchBackend
 
+def get_test_qaoa_tn(n=10, p=2, d=3, type='random'):
+    G = qtensor.toolbox.random_graph(seed=10, degree=d, nodes=n, type=type)
+    print('Test problem: n, p, d', n, p, d)
+    gamma, beta = [np.pi/5]*p, [np.pi/2]*p
+
+    composer = qtensor.DefaultQAOAComposer(
+        graph=G, gamma=gamma, beta=beta)
+    composer.ansatz_state()
+    tn = qtensor.optimisation.TensorNet.QtreeTensorNet.from_qtree_gates(composer.circuit)
+    return tn
+
+def test_torch_get_sliced__slice():
+    backend = TorchBackend()
+    tn = get_test_qaoa_tn()
+    tensor = tn.buckets[0][0]
+    tensor_data = tn.data_dict[tensor.data_key]
+    slice_dict = {
+        tensor.indices[0]: slice(0, 1)
+    }
+    buckets = backend.get_sliced_buckets(
+        tn.buckets, tn.data_dict, slice_dict
+    )
+    assert tensor_data.shape != buckets[0][0].data.shape
+
+def test_torch_get_sliced__noslice():
+    backend = TorchBackend()
+    tn = get_test_qaoa_tn()
+
+    tensor = tn.buckets[0][0]
+    tensor_data = tn.data_dict[tensor.data_key]
+    buckets = backend.get_sliced_buckets(
+        tn.buckets, tn.data_dict, {}
+    )
+    assert len(buckets) == len(tn.buckets)
+    assert tensor_data.shape == buckets[0][0].data.shape
 
 def test_torch_get_sliced_smoke():
     backend = TorchBackend()
--- qtensor/contraction_backends/torch.py
+++ qtensor/contraction_backends/torch.py
@@ -2,6 +2,16 @@ import torch
 from qtree import np_framework
 
 from qtensor.contraction_backends import ContractionBackend
+def qtree2torch_tensor(tensor, data_dict):
+    """ Converts qtree tensor to pytorch tensor using data dict"""
+    if tensor.data is not None:
+        data = tensor.data
+    else:
+        data = data_dict[tensor.data_key]
+    torch_t = torch.Tensor(data)
+    data_dict[tensor.data_key] = torch_t
+    return tensor.copy(data=torch_t)
+
 
 class TorchBackend(ContractionBackend):
     def process_bucket(self, bucket, no_sum=False):
@@ -10,7 +20,7 @@ class TorchBackend(ContractionBackend):
     def get_sliced_buckets(self, buckets, data_dict, slice_dict):
         np_buckets = np_framework.get_sliced_np_buckets(buckets, data_dict, slice_dict)
         torch_buckets = [
-            [torch.Tensor(x) for x in bucket]
+            [qtree2torch_tensor(x, data_dict) for x in bucket]
             for bucket in np_buckets
         ]
         return torch_buckets
