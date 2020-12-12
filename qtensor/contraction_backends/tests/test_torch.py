ee43ccbda6a9c60f7b9a3d023b62baa2e9728edc
Danil Lykov <lkv97dn@gmail.com>
2020-12-12 03:00:10 -0600

    add torch contract buckets and tests for it

--- qtensor/contraction_backends/tests/test_torch.py
+++ qtensor/contraction_backends/tests/test_torch.py
@@ -1,6 +1,7 @@
 import qtensor
+import torch
 import numpy as np
-from qtensor.contraction_backends import TorchBackend
+from qtensor.contraction_backends import TorchBackend, NumpyBackend
 
 def get_test_qaoa_tn(n=10, p=2, d=3, type='random'):
     G = qtensor.toolbox.random_graph(seed=10, degree=d, nodes=n, type=type)
@@ -12,6 +13,40 @@ def get_test_qaoa_tn(n=10, p=2, d=3, type='random'):
     composer.ansatz_state()
     tn = qtensor.optimisation.TensorNet.QtreeTensorNet.from_qtree_gates(composer.circuit)
     return tn
+
+def test_torch_process_bucket():
+    btr = TorchBackend()
+    bnp = NumpyBackend()
+    def contract_tn(backend, search_len=1, test_problem_kwargs={}):
+        """
+        search_len is used to select non-trivial buckets.
+        test_problem_kwargs is used to generate custom graph for test_problem
+        """
+        tn = get_test_qaoa_tn(**test_problem_kwargs)
+        sliced_buckets = backend.get_sliced_buckets(tn.buckets, tn.data_dict, {})
+        good_buckets = [x for x in sliced_buckets if len(x) >= search_len]
+        if not good_buckets:
+            raise ValueError('Could not find large enough buckets')
+        selected_bucket = good_buckets[0]
+        print('selected_bucket', selected_bucket)
+
+        result = backend.process_bucket(selected_bucket)
+        return result.data
+
+    # First test only simple buckets
+    restr = contract_tn(btr, 1)
+    resnp = contract_tn(bnp, 1)
+    assert type(restr) is torch.Tensor
+    assert restr.dtype is torch.cfloat
+
+    assert np.allclose(restr, resnp)
+
+    # Then test more advanced
+    restr = contract_tn(btr, 2, dict(n=10, p=4, d=3))
+    resnp = contract_tn(bnp, 2, dict(n=10, p=4, d=3))
+
+    assert restr.shape == resnp.shape
+    assert np.allclose(restr, resnp)
 
 def test_torch_get_sliced__slice():
     backend = TorchBackend()
--- qtensor/contraction_backends/torch.py
+++ qtensor/contraction_backends/torch.py
@@ -1,4 +1,5 @@
 from qtensor.tools.lazy_import import torch
+import qtree
 from qtree import np_framework
 
 from qtensor.contraction_backends import ContractionBackend
@@ -8,14 +9,47 @@ def qtree2torch_tensor(tensor, data_dict):
         data = tensor.data
     else:
         data = data_dict[tensor.data_key]
-    torch_t = torch.Tensor(data)
+    torch_t = torch.from_numpy(data)
     data_dict[tensor.data_key] = torch_t
     return tensor.copy(data=torch_t)
 
 
 class TorchBackend(ContractionBackend):
     def process_bucket(self, bucket, no_sum=False):
-        raise NotImplementedError
+        result_indices = bucket[0].indices
+        result_data = bucket[0].data
+        for tensor in bucket[1:]:
+
+            expr = qtree.utils.get_einsum_expr(
+                list(map(int, result_indices)), list(map(int, tensor.indices))
+            )
+
+            result_data = torch.einsum(expr, result_data, tensor.data)
+
+            # Merge and sort indices and shapes
+            result_indices = tuple(sorted(
+                set(result_indices + tensor.indices),
+                key=int)
+            )
+
+        if len(result_indices) > 0:
+            if not no_sum:  # trim first index
+                first_index, *result_indices = result_indices
+            else:
+                first_index, *_ = result_indices
+            tag = first_index.identity
+        else:
+            tag = 'f'
+            result_indices = []
+
+        # reduce
+        if no_sum:
+            result = qtree.optimizer.Tensor(f'E{tag}', result_indices,
+                                data=result_data)
+        else:
+            result = qtree.optimizer.Tensor(f'E{tag}', result_indices,
+                                data=torch.sum(result_data, axis=0))
+        return result
 
     def get_sliced_buckets(self, buckets, data_dict, slice_dict):
         np_buckets = np_framework.get_sliced_np_buckets(buckets, data_dict, slice_dict)
@@ -26,9 +60,4 @@ class TorchBackend(ContractionBackend):
         return torch_buckets
 
     def get_result_data(self, result):
-        raise NotImplementedError
-
-def test_torch_backend():
-    print('Hello world')
-    assert False
-
+        return result.data
