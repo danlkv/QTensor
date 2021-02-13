4bad39eda5770130ce6b66171e286a60f184225c
Danil Lykov <lkv97dn@gmail.com>
2021-02-13 15:17:35 -0600

    add backend picking function

--- qtensor/contraction_backends/__init__.py
+++ qtensor/contraction_backends/__init__.py
@@ -5,3 +5,12 @@ from .mkl import CMKLExtendedBackend
 from .transposed import TransposedBackend
 from .opt_einsum import OptEinusmBackend
 from .performance_measurement_decorator import PerfNumpyBackend
+
+def get_backend(name):
+    return {
+        'einsum':NumpyBackend,
+        'torch': TorchBackend,
+        'mkl':CMKLExtendedBackend,
+        'tr_einsum': TransposedBackend,
+        'opt_einsum': OptEinusmBackend,
+    }[name]
