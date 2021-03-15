f047e0e69e2ab1b72fc2008342099d6e8d6f25c6
Ruslan Shaydulin <rshaydu@g.clemson.edu>
2021-03-15 08:46:29 -0400

    added the test from pull request thread to tests folder

---
+++ qtensor/tests/test_lightcone_orbits.py
@@ -0,0 +1,16 @@
+import networkx as nx
+import numpy as np
+from qtensor import QAOA_energy, QAOAQtreeSimulatorSymmetryAccelerated, QtreeQAOAComposer
+
+def test_lightcone_energy_value():
+    G = nx.random_regular_graph(3, 100, seed=42)
+    gamma, beta = [np.pi/3], [np.pi/2]
+    
+    E = QAOA_energy(G, gamma, beta)
+    
+    sym = QAOAQtreeSimulatorSymmetryAccelerated(QtreeQAOAComposer)
+    
+    E2 = sym.energy_expectation(G, gamma, beta)
+    
+    assert(np.isclose(E,E2))
+    
