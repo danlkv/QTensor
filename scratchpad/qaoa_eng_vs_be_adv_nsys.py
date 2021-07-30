2b69394490aaa5c5b707329164f20e99e733009e
huaxuan250 <huaxuan250@gmail.com>
2021-07-30 11:27:55 -0500

    Update Profile Specs

--- scratchpad/qaoa_eng_vs_be_adv_nsys.py
+++ scratchpad/qaoa_eng_vs_be_adv_nsys.py
@@ -7,7 +7,7 @@ import pyrofiler
 import qtensor
 from qtensor import QtreeQAOAComposer
 from qtensor import QAOAQtreeSimulator
-from qtensor.contraction_backends import get_backend
+from qtensor.contraction_backends import get_backend, get_perf_backend
 
 
 timing = pyrofiler.timing
@@ -42,7 +42,7 @@ def get_gpu_props_json():
         return None
 
 paramtest = [
-    [24,4,3,"random"]
+    [8,3,3,"random"] # 8 is the sweet spot
     ,[4, 4, 3, 'random']
     ,[10, 5, 2, 'random']
     ,[14, 1, 3, 'random']
@@ -90,18 +90,18 @@ CHANGE: Rid aggregation methods
 '''
 def gen_be_lc_report(G, gamma, beta, edge, peo, backend_name, gen_base = 0):
 
-    curr_backend = get_backend(backend_name)
+    curr_backend = get_perf_backend(backend_name)
     curr_sim = QAOAQtreeSimulator(QtreeQAOAComposer,backend=curr_backend)
     circuit = curr_sim._edge_energy_circuit(G, gamma, beta, edge)
     curr_sim.simulate_batch(circuit, peo = peo)
-    #curr_sim.backend.gen_report(show = False)
+    curr_sim.backend.gen_report(show = True)
 
 
 
 if __name__ == '__main__':
     gen_sim = QAOAQtreeSimulator(QtreeQAOAComposer)
     backends = ["cupy","torch_gpu","einsum","torch","tr_einsum","opt_einsum"]
-    my_algo = 'rgreedy_0.05_30'
+    my_algo = 'greedy'
     for pb in [paramtest[0]]:
 
         '''
@@ -114,9 +114,9 @@ if __name__ == '__main__':
 
         gen_base = gen_pb.result
         agg_reports = []
-        for be in [backends[5]]:
+        for be in [backends[0]]:
             all_lightcones_report = []
             for i, pack in enumerate(zip(G.edges, peos)):
                 edge, peo = pack
-                for _ in range(3):
+                for _ in range(5):
                     gen_be_lc_report(G, gamma, beta, edge, peo, be)
