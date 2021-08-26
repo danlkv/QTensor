d8cd1378f15f991d9f4a1249122316b8fd49f3fe
Danil Lykov <lkv97dn@gmail.com>
2021-08-25 20:50:50 -0500

    try to not skip pynauty

--- qtensor/tests/test_lightcone_orbits.py
+++ qtensor/tests/test_lightcone_orbits.py
@@ -3,7 +3,7 @@ import pytest
 import numpy as np
 from qtensor import QAOA_energy, QAOAQtreeSimulatorSymmetryAccelerated, QtreeQAOAComposer
 
-@pytest.mark.skip(reason='pynauty throws "Illegal instruction" on gh actions')
+#@pytest.mark.skip(reason='pynauty throws "Illegal instruction" on gh actions')
 def test_parallel_lightcone():
     from cartesian_explorer import parallels
     from qtensor.tools import lightcone_orbits
@@ -11,7 +11,7 @@ def test_parallel_lightcone():
     G = nx.random_regular_graph(3, 1000, seed=42)
     eorbits_dict = lightcone_orbits.get_edge_orbits_lightcones(G, p=3, nprocs=par)
 
-@pytest.mark.skip(reason='pynauty throws "Illegal instruction" on gh actions')
+#@pytest.mark.skip(reason='pynauty throws "Illegal instruction" on gh actions')
 def test_lightcone_energy_value():
     G = nx.random_regular_graph(3, 100, seed=42)
     gamma, beta = [np.pi/3], [np.pi/2]
@@ -24,7 +24,7 @@ def test_lightcone_energy_value():
 
     assert(np.isclose(E,E2))
 
-@pytest.mark.skip(reason='pynauty throws "Illegal instruction" on gh actions')
+#@pytest.mark.skip(reason='pynauty throws "Illegal instruction" on gh actions')
 def test_lightcone_energy_value_large():
     # large graph should trigger parallel evaluation of lightcone orbits
     G = nx.random_regular_graph(3, 1000, seed=42)
