c78e4fba183ed799d139ccd4fa629f2f19f08de1
huaxuan250 <huaxuan250@gmail.com>
2022-02-08 22:39:04 -0600

    Load Detect Focus on maxwidth

--- scratchpad/bench/circontraction/qaoa_eng_load_detect.py
+++ scratchpad/bench/circontraction/qaoa_eng_load_detect.py
@@ -1,18 +1,17 @@
 from functools import lru_cache
-from matplotlib import backend_bases
 import numpy as np
 import networkx as nx
-import platform
-import pyrofiler
 import qtensor
 from qtensor import QtreeQAOAComposer
 from qtensor import QAOAQtreeSimulator
-from qtensor import toolbox
 from qtensor.contraction_backends import get_mixed_backend
 import pyrofiler.c as c
 import pandas as pd
 from functools import reduce
+import random
 
+random.seed(42)
+np.random.seed(42)
 
 @lru_cache
 def get_test_problem(n=10, p=2, d=3, type='random'):
@@ -35,14 +34,28 @@ def gen_QAOA_circs(G, gamma, beta):
         circs.append(circ)
     return circs
 
+
 def gen_circs_peos(circuits, algo):
     peos = []
+    widths = []
     for circ  in circuits:
         tn = qtensor.optimisation.TensorNet.QtreeTensorNet.from_qtree_gates(circ)
         opt = qtensor.toolbox.get_ordering_algo(algo)
         peo, _ = opt.optimize(tn)
+        treeWidth = opt.treewidth
         peos.append(peo)
-    return peos
+        widths.append(treeWidth)
+    return peos, widths
+
+'''
+TODO: a function to find the max peos and circ
+'''
+def get_max_peo_n_circ(circuits, peos, widths):
+    max_width = max(widths)
+    print("Max Tree Width: {}".format(max_width))
+    max_index = widths.index(max_width)
+    return circuits[max_index], peos[max_index]
+
 
 def gen_circ_peo_report_mix(circuit, peo, backend_name, threshold):
 
@@ -52,7 +65,7 @@ def gen_circ_peo_report_mix(circuit, peo, backend_name, threshold):
 
 
 paramtest = [
-    [20,4,3,"random"]
+    [24,4,3,"random"]
 ]
 
 def process_one_series(result):
@@ -81,9 +94,9 @@ def df2reduced(collections):
 
 
 if __name__ == '__main__':
-    backends = [['einsum','cupy']]
+    backends = [['einsum','torch_gpu']]
     threshold = [16]
-    my_reap = 3
+    my_reap = 5
     my_algo = 'greedy'
     
     for thr in threshold:
@@ -92,23 +105,24 @@ if __name__ == '__main__':
             n, p, d, ttype = pb
             G, gamma, beta = get_test_problem(n,p,d,ttype)
             circs = gen_QAOA_circs(G, gamma, beta)
-            peos = gen_circs_peos(circs,my_algo)
+            peos, widths = gen_circs_peos(circs,my_algo)
 
+            maxCirc, maxPeo = get_max_peo_n_circ(circs, peos, widths)
             for be in backends:
                 cpus = []
                 gpus = []
                 for _ in range(my_reap):
                     with c.cpu_util_hist() as c_hist:
                         with c.gpu_util_hist() as g_hist:
-                            for i, pack in enumerate(zip(circs, peos)):
-                                circ, peo = pack
-                                gen_circ_peo_report_mix(circ, peo, be, thr)
+                            gen_circ_peo_report_mix(maxCirc, maxPeo, be, thr)
+
+
                     raw_cpu = process_one_series(c_hist.result)
                     cooked_cpu = raw2df(raw_cpu, '500ms')
                     cpus.append(cooked_cpu)
 
                     raw_gpu = process_one_series(g_hist.result)
-                    cooked_gpu = raw2df(raw_gpu, '500ms')
+                    cooked_gpu = raw2df(raw_gpu, '10ms')
                     gpus.append(cooked_gpu)
                 reduced_cpu = df2reduced(cpus)
                 reduced_gpu = df2reduced(gpus)
