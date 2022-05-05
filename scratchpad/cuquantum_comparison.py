aee146c45e617ba92c994e55262933395f9ad4fe
Danil Lykov <lkv97dn@gmail.com>
2022-05-05 13:50:32 -0500

    switch cuquantum comparison to gpu backend

--- scratchpad/cuquantum_comparison.py
+++ scratchpad/cuquantum_comparison.py
@@ -8,7 +8,7 @@ import re
 from quimb.tensor.tensor_core import concat, _gen_output_inds, _inds_to_eq
 
 N = 32
-p = 2
+p = 4
 gamma, beta = [.2]*p, [.3]*p
 G = nx.random_regular_graph(3, N)
 
@@ -32,7 +32,7 @@ print('QTensor = ', end='', flush=True)
 comp = qt.DefaultQAOAComposer(G, gamma=gamma, beta=beta)
 comp.ansatz_state()
 tn = qt.optimisation.QtreeTensorNet.from_qtree_gates(comp.circuit)
-opt = qt.toolbox.get_ordering_algo('rgreedy_0.02_20')
+opt = qt.toolbox.get_ordering_algo('tamaki_5')
 start = time.time()
 peo, tw = opt.optimize(tn)
 end = time.time()
@@ -50,31 +50,34 @@ tdata = [t.data for t in qu_tn.tensors]
 # Turn cuquantum logging on.
 import logging
 #logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', force=True )
+network = cq.Network(eq, *tdata)
 
 # Compute the path.
 # 1. Disable slicing for this problem.
 # 2. Use 16 hyperoptimizer samples. 
 start = time.time()
 slicer_opt = cq.SlicerOptions(disable_slicing=True)
-path, info = cq.contract_path(eq, *tdata, optimize={'samples' : 16, 'slicing': slicer_opt})
+reconf_opt = cq.ReconfigOptions(num_iterations=0)   
+samples, threads = 16, 2
+path, info = network.contract_path(optimize={'samples' : samples, 'threads' : threads,  'slicing': slicer_opt, 'reconfiguration' : reconf_opt})
 end = time.time()
 
 print(f"{info.opt_cost}, largest intermediate = {info.largest_intermediate} Elements")
 print('CuQuantum hyper path finding time = ', end-start)
 
 # ---- Vanilia Cuquantum
-cq_r = cq.einsum_path(eq, *tdata)
+#cq_r = cq.einsum_path(eq, *tdata)
 
 #el =  re.search('Cuquantum vanilia: Largest intermediate = ?(.+)', cq_r[1]).groups()[0]
 #print(el)
 
 print('Simulation:')
 
-qu_c = qu.tensor.circuit_gen.circ_qaoa(terms, p, gamma, beta)
+#qu_c = qu.tensor.circuit_gen.circ_qaoa(terms, p, gamma, beta)
 print('Quimb = ', end='', flush=True)
 
 start = time.time()
-qu_r = qu_c.amplitude(''.join(['0']*N))
+#qu_r = qu_c.amplitude(''.join(['0']*N))
 end = time.time()
 print(end-start)
 
@@ -83,6 +86,16 @@ print('QTensor *contraction only* time = ', end='', flush=True)
 comp = qt.DefaultQAOAComposer(G, gamma=gamma, beta=beta)
 comp.ansatz_state()
 sim = qt.QtreeSimulator(optimizer=opt)
+start = time.time()
+sim.simulate_batch(comp.circuit, peo=peo)
+end = time.time()
+
+print(end - start)
+
+print('QTensor *contraction only* GPU time = ', end='', flush=True)
+comp = qt.DefaultQAOAComposer(G, gamma=gamma, beta=beta)
+comp.ansatz_state()
+sim = qt.QtreeSimulator(optimizer=opt, backend=qt.contraction_backends.TorchBackend())
 start = time.time()
 sim.simulate_batch(comp.circuit, peo=peo)
 end = time.time()
