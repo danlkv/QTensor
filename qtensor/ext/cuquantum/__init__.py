32b75ce992557bf3565c0199f5798ea21aee17ff
Danil Lykov <lkv97dn@gmail.com>
2022-05-18 21:33:06 -0500

    add cuquantum optimizer and simulator

--- qtensor/ext/cuquantum/__init__.py
+++ qtensor/ext/cuquantum/__init__.py
@@ -1,6 +1,7 @@
 # -- Lazy import of cuquantum package
 from qtensor.tools.lazy_import import LasyModule
 cuquantum = LasyModule('cuquantum')
+import numpy as np
 cq = cuquantum # just a shorthand alias
 # --
 
@@ -27,9 +28,16 @@ class CuTensorNet(TensorNet):
         qtr_net = qopt.QtreeTensorNet.from_qtree_gates(qc, init_state, **kwargs)
         return cls.from_qtree_tn(qtr_net)
 
+    @staticmethod
+    def get_tensor_data(tensor, data_dict):
+        if hasattr(tensor, 'data'):
+            if tensor.data is not None:
+                return tensor.data 
+        return data_dict[tensor.data_key]
+
     @classmethod
     def from_qtree_tn(cls, qtr_net, dtype='complex64'):
-        all_data = [qtr_net.data_dict[t.data_key] for t in qtr_net.tensors]
+        all_data = [cls.get_tensor_data(t, qtr_net.data_dict) for t in qtr_net.tensors]
         var2char = lambda x: get_symbol(x.identity)
         all_indices = [''.join([var2char(i) for i in t.indices]) for t in qtr_net.tensors]
         eq_lh = ','.join(all_indices)
@@ -39,3 +47,64 @@ class CuTensorNet(TensorNet):
         return cls(eq, *tdata)
         
 
+
+# -- Cuquantum Optimizer
+
+class CuQuantumOptimizer(qopt.Optimizer):
+    def __init__(self, **kwargs):
+        """
+        Args:
+            samples (int): number of samples, scales time
+            threads (int): number of threads
+            slicing (cuquantum.SlicerOptions): slicing options
+            reconfiguration (cuquantum.ReconfigOptions): reconfiguration options
+        """
+        self.kwargs = kwargs
+        self.kwargs['samples'] = kwargs.get('samples', 10)
+        self.kwargs['threads'] = kwargs.get('threads', 1)
+        self.kwargs['slicing'] = kwargs.get('slicing', cq.SlicerOptions())
+        self.kwargs['reconfiguration'] = kwargs.get('reconfiguration',
+                                                    cq.ReconfigOptions(num_iterations=0))
+
+    @property
+    def peo(self):
+        raise Exception("CuQuantumOptimizer does not provide elimination order."
+                        " Use with compatible simulators")
+
+    def optimize(self, tensor_net):
+        path, info = tensor_net.net.contract_path(optimize=self.kwargs)
+        self.treewidth = int(np.log2(info.largest_intermediate))
+        return path, tensor_net
+
+# -- Cuquantum Simulator
+
+from qtensor import QtreeSimulator
+
+class CuQuantumSimulator(QtreeSimulator):
+    def __init__(self, optimizer=None, max_tw=None):
+        self.max_tw = max_tw
+        if optimizer:
+            self.optimizer = optimizer
+        else:
+            self.optimizer = CuQuantumOptimizer()
+
+    def simulate_batch(self, qc, batch_vars=0, peo=None):
+        self._new_circuit(qc)
+        self.tn = qopt.QtreeTensorNet.from_qtree_gates(self.all_gates)
+        if isinstance(batch_vars, int):
+            free_final_qubits = list(range(batch_vars))
+        else:
+            free_final_qubits = batch_vars
+        self._set_free_qubits(free_final_qubits)
+        slice_dict = self._get_slice_dict()
+        self.tn.slice(slice_dict)
+
+        qtn = CuTensorNet.from_qtree_tn(self.tn)
+        if peo is None:
+            path, _ = self.optimizer.optimize(qtn)
+            if self.max_tw:
+                if self.optimizer.treewidth > self.max_tw:
+                    raise ValueError(f'Treewidth {self.optimizer.treewidth} is larger than max_tw={self.max_tw}.')
+        else:
+            path = peo
+        return cq.contract(qtn._eq, *qtn.tensors, optimize={'path': path})
--- qtensor/ext/cuquantum/test_cuquantum_ext.py
+++ qtensor/ext/cuquantum/test_cuquantum_ext.py
@@ -1,6 +1,6 @@
 from qtensor.ext import cuquantum as cq
 from qtensor.tests import get_test_problem
-from qtensor import DefaultQAOAComposer
+from qtensor import DefaultQAOAComposer, QtreeSimulator
 import random, numpy as np
 
 random.seed(10)
@@ -11,7 +11,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
 format='%(asctime)s %(levelname)-8s %(message)s', force=True )
 
-def test_cuquantum():
+def test_cuquantum_tn():
     G, gamma, beta = get_test_problem(16, 3, d=3)
     composer = DefaultQAOAComposer(
             graph=G, gamma=gamma, beta=beta)
@@ -32,3 +32,43 @@ def test_cuquantum():
             reconfiguration=reconf_opt
         )
     )
+
+def test_cuquantum_opt():
+    G, gamma, beta = get_test_problem(16, 3, d=3)
+    composer = DefaultQAOAComposer(
+            graph=G, gamma=gamma, beta=beta)
+    composer.ansatz_state()
+    circ = composer.circuit
+    tn = cq.CuTensorNet.from_qtree_gates(circ)
+    slicer_opt = cq.cq.SlicerOptions()
+    print('Cuquantum equation', tn._eq)
+    print('Cuquantum tensor count', len(tn))
+    opt = cq.CuQuantumOptimizer(slicing=slicer_opt, threads=1, samples=10)
+    path, tn = opt.optimize(tn)
+    print('Width cuquantum', opt.treewidth)
+
+def test_cuquantum_sim():
+    G, gamma, beta = get_test_problem(16, 4, d=3)
+    composer = DefaultQAOAComposer(
+            graph=G, gamma=gamma, beta=beta)
+    composer.ansatz_state()
+    circ = composer.circuit
+    sim = cq.CuQuantumSimulator()
+    res = sim.simulate(circ)
+
+    sim2 = QtreeSimulator()
+    res2 = sim2.simulate(circ)
+    assert np.allclose(res, res2)
+
+def test_cuquantum_sim_batch():
+    G, gamma, beta = get_test_problem(16, 4, d=3)
+    composer = DefaultQAOAComposer(
+            graph=G, gamma=gamma, beta=beta)
+    composer.ansatz_state()
+    circ = composer.circuit
+    sim = cq.CuQuantumSimulator()
+    res = sim.simulate_batch(circ, batch_vars=6)
+
+    sim2 = QtreeSimulator()
+    res2 = sim2.simulate_batch(circ, batch_vars=6)
+    assert np.allclose(res.flatten(), res2)
