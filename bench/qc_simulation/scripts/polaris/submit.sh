310e74a5db9f4bcaff5582abe6c03defa43136a7
Dan Lykov <dlykov@anl.gov>
2023-04-05 02:09:17 +0000

    update submit script

--- bench/qc_simulation/scripts/polaris/submit.sh
+++ bench/qc_simulation/scripts/polaris/submit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
 
-NODES=256
+NODES=2
 RANKS=$(( NODES * 4 ))
-QUEUE=prod
-WALLTIME=420:00
+QUEUE=debug-scaling
+WALLTIME=40:00
 
 qsub -l select=$NODES:system=polaris:ncpus=32:ngpus=4:gputype=A100,walltime=$WALLTIME,filesystems=home \
-    -q $QUEUE -ACatalyst \
+    -q $QUEUE -AQTensor \
     -v RANKS=$RANKS,PARAM_P=$PARAM_P \
     -o job_out.output -e job_out.output \
-    ./entry.sh
+    ./scripts/polaris/entry.sh
 
 echo -e "===========\nNew job with NODES=$NODES, PARAM_P=$PARAM_P submitted.\n" >> job_out.output
 sleep 1.5
