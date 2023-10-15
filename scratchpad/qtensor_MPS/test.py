7a2cb6bfe2c57a6d0dee15dddb29c3bd519624a4
tina oberoi <toberoi@tinas-MacBook-Air.local>
2023-10-15 16:40:14 -0500

    Add tests for ghz and bell

--- scratchpad/qtensor_MPS/test.py
+++ scratchpad/qtensor_MPS/test.py
@@ -8,11 +8,6 @@ def test_from_wavefunction_all_zero_state():
     wavefunction = np.array([1, 0, 0, 0, 0, 0, 0, 0])
     mps = MPS.construct_mps_from_wavefunction(wavefunction, 'q', 3, 2)
 
-    # kwargs.setdefault("legend", True)
-    # kwargs.setdefault("compass", True)
-    # kwargs.setdefault("compass_labels", mps.inds)
-    # xyz.visualize_tensor(mps.get_tensors(False), legend=True, compass=True)
-
     assert isinstance(mps, MPS)
     assert mps.N == 3
     assert mps.physical_dim == 2
@@ -24,48 +19,27 @@ def test_from_wavefunction_random():
     wavefunction = wavefunction / np.linalg.norm(wavefunction, ord = 2)
     mps = MPS.construct_mps_from_wavefunction(wavefunction, 'q', n, 2)
     assert np.allclose(mps.get_wavefunction(), wavefunction)
-#def add random function
-#  
-
-# Entangled
 
 def test_apply_one_qubit_mps_operation_xgate():
     mps = MPS("q", 2, 2)
-    print("Before applying gate: ", mps.get_wavefunction())
-
-    # nodes = mps.get_tensors(False)
-    # qubits = [node[0] for node in nodes]
     # q0q1 = 00
     # On apply x gate |00> -> |10>
     mps.apply_single_qubit_gate(xgate(), 0)
-    
-    print("After applying x gate: ", mps.get_wavefunction())
+    assert np.allclose(mps.get_wavefunction(), np.array([0.0, 0.0, 1.0, 0.0], dtype=np.complex64))
 
 def test_apply_twoq_cnot_two_qubits():
-    """Tests for correctness of final wavefunction after applying a CNOT
-    to a two-qubit MPS.
-    """
     # In the following tests, the first qubit is always the control qubit.
     # Check that CNOT|10> = |11>
     mps = MPS("q", 2, 2)
     mps.apply_single_qubit_gate(xgate(), 0)
-
-    print("After applying x gate: ", mps.get_wavefunction())
-
     mps.apply_two_qubit_gate(cnot(), [0, 1])
-    print("After applying cnot gate: ", mps.get_wavefunction())
-
-
-    correct = np.array([0.0, 0.0, 0.0, 1.0], dtype=np.complex64)
-    curr = mps.get_wavefunction()
-    # assert np.allclose(curr, correct)
+    assert np.allclose(mps.get_wavefunction(), np.array([0.0, 0.0, 0.0, 1.0], dtype=np.complex64))
 
 def test_apply_gate_for_bell_circuit():
     mps = MPS("q", 2, 2)
     mps.apply_single_qubit_gate(hgate(), 0)
     mps.apply_two_qubit_gate(cnot(), [0,1])
-
-    print("Wavefunction for bell circuit: ", mps.get_wavefunction())
+    assert np.allclose(mps.get_wavefunction(), np.array([0.707, 0.0, 0.0, 0.707], dtype=np.complex64))
 
 def test_apply_gate_for_ghz_circuit():
     mps = MPS("q", 3, 2)
@@ -73,13 +47,4 @@ def test_apply_gate_for_ghz_circuit():
     mps.apply_two_qubit_gate(cnot(), [0,1])
     mps.apply_two_qubit_gate(cnot(), [1,2])
 
-    print("Wavefunction for ghz circuit: ", mps.get_wavefunction())
-
-# test_from_wavefunction_all_zero_state()
-# test_apply_one_qubit_mps_operation_xgate()
-# test_from_wavefunction_random()
-
-#test_apply_twoq_cnot_two_qubits()
-
-# test_apply_gate_for_bell_circuit()
-test_apply_gate_for_ghz_circuit()
+    assert np.allclose(mps.get_wavefunction(), np.array([ 0.7071, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.7071], dtype=np.complex64))
