671216f965a0cb2652ffe8177fa6b3be47511749
tinaoberoi <oberoi011115tina@gmail.com>
2023-10-30 00:23:53 -0500

    Add evolution code

--- scratchpad/qtensor_MPS/evolution.py
+++ scratchpad/qtensor_MPS/evolution.py
@@ -4,41 +4,44 @@ from mps import MPS
 from gates import *
 import matplotlib.pyplot as plt
 
-N = 10
 cutoff = 1e-8
 tau = 0.1
 ttotal = 2.0
 
 gates = []
-N = 25
-n = int(N/2)
-mps = MPS("q", N, 2)
+N = 10
+n = N
+mps = MPS("q", N+1, 2)
 evolution_range = np.linspace(0, 1, 5)
 js = np.arange(0, n)
 
 magnetization = []
 for t in evolution_range:
-    mag_t = []
-    for j in range(0, n):
+    mag_j = []
+
+    for j in range(0, N):
         mps.apply_two_qubit_gate(isingHamiltonian(t), [j, j+1])
 
-        mag_t += [mps.get_expectation(zgate(), j)]
+    for j in range(N, 0):
+        mps.apply_two_qubit_gate(isingHamiltonian(t), [j-1, j])
 
-    magnetization += [mag_t]
+    for j in range(0, N):
+        mag_j += [mps.get_expectation(zgate(), j)]
+    
+    magnetization += [mag_j]
 
-with plt.style.context('default'):
-    plt.figure(figsize=(12, 7))
+plt.style.context('default')
+plt.figure(figsize=(12, 7))
 
-    # plot the magnetization
-    ax1 = plt.subplot(131)
-    plt.pcolormesh(js, evolution_range, np.real(magnetization))
-    plt.set_cmap('RdYlBu')
-    plt.colorbar()
-    plt.title('Z-Magnetization')
-    plt.xlabel('Site')
-    plt.ylabel('time [ $Jt$ ]')
+# plot the magnetization
+plt.pcolormesh(js, evolution_range, np.real(magnetization), vmin=-0.5, vmax=0.5)
+plt.set_cmap('RdYlBu')
+plt.colorbar()
+plt.title('Z-Magnetization')
+plt.xlabel('Site')
+plt.ylabel('time [ $Jt$ ]')
 
-    plt.show()
+plt.savefig('magnetisation.png')
 
 
 
