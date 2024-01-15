8583a1adcb62fe1055a3e1f5d8c9cbf4ae089aa3
tinaoberoi <oberoi011115tina@gmail.com>
2024-01-15 17:49:05 -0600

    Add evolution of mpo

--- scratchpad/qtensor_MPS/evolution_mpo.py
+++ scratchpad/qtensor_MPS/evolution_mpo.py
@@ -8,53 +8,57 @@ import matplotlib.pyplot as plt
 ## See the mpo prepared and at the end the zgate thing
 ## Compare resuts
 cutoff = 1e-8
-N = 10
+N = 5
 mps = MPS("q", N + 1, 2)
 mps.apply_single_qubit_gate(xgate(), 0)
-mps.apply_single_qubit_gate(xgate(), 9)
+# mps.apply_single_qubit_gate(xgate(), 4)
 assert mps.get_norm() == 1
-print(mps.get_wavefunction())
+# print(mps.get_wavefunction())
 
 mpo = MPOLayer("q", N + 1, 2)
-pauli_string = "Z" * (N + 1)
-mpo.construct_mpo(pauli_string)
-evolution_range = np.linspace(0, 5, 20)
+evolution_range = np.linspace(0, 5, 30)
 js = np.array(range(N))
 
 magnetization = []
 
 mag_j = []
 
+# for j in range(0, N):
+#     mpo.add_single_qubit_gate(zgate(), j)
+#     mag_j += [mpo.mpo_mps_inner_prod(mps)]
+#     mpo.add_single_qubit_gate(zgate(), j)
+
+# magnetization += [mag_j]
+
+dt = 25 / 20
+
 for j in range(0, N):
-    mag_j += [mps.get_expectation(zgate(), j)]
-
-magnetization += [mag_j]
-
-dt = 20 / 20
-
-for t in evolution_range:
+    mpo = MPOLayer("q", N + 1, 2)
+    mpo.add_single_qubit_gate(zgate(), j)
     mag_j = []
+    mag_j += [mpo.mpo_mps_inner_prod(mps)]
+    for t in evolution_range:
+        # for j in js:
+        # mpo.add_two_qubit_gate(sigmaRzz(dt), [j, j + 1])
+        # mpo.add_two_qubit_gate(sigmaRzz(dt), [j, j + 1], True)
 
-    for j in js:
-        mpo.add_two_qubit_gate(sigmaRzz(dt), [j, j + 1])
-        mpo.add_two_qubit_gate(sigmaRzz(dt), [j, j + 1], True)
+        for j in js:
+            mpo.add_single_qubit_gate(sigmaRx(dt), j)
+            mpo.add_single_qubit_gate(sigmaRx(dt), j, True)
 
-    for j in js:
-        mpo.add_single_qubit_gate(sigmaRx(dt), j)
-        mpo.add_single_qubit_gate(sigmaRx(dt), j, True)
+        # for j in js:
+        #     mpo.add_two_qubit_gate(sigmaRzz(-1 * t), [N - 1 - j, N - j])
+        #     mpo.add_two_qubit_gate(sigmaRzz(-1 * t), [N - 1 - j, N - j], True)
 
-    for j in js:
-        mpo.add_two_qubit_gate(sigmaRzz(-1 * t), [N - 1 - j, N - j])
-        mpo.add_two_qubit_gate(sigmaRzz(-1 * t), [N - 1 - j, N - j], True)
+        # for j in js:
+        #     mpo.add_single_qubit_gate(sigmaRx(-1 * t), N - 1 - j)
+        #     mpo.add_single_qubit_gate(sigmaRx(-1 * t), N - 1 - j, True)
 
-    for j in js:
-        mpo.add_single_qubit_gate(sigmaRx(-1 * t), N - 1 - j)
-        mpo.add_single_qubit_gate(sigmaRx(-1 * t), N - 1 - j, True)
+        # mps.apply_mpo_layer(mpo)
+        # ZIIIII..
+        # IZIIII...
+        # IIZIII...
 
-    # # ZIIIII..
-    # # IZIIII...
-    # # IIZIII...
-    for j in range(0, N):
         mag_j += [mpo.mpo_mps_inner_prod(mps)]
 
     magnetization += [mag_j]
@@ -63,11 +67,14 @@ plt.style.context("default")
 plt.figure(figsize=(12, 7))
 
 # plot final magnetization
-plt.pcolormesh(js, np.linspace(0, 5, 21), np.real(magnetization))
+plt.pcolormesh(js, np.linspace(0, 5, 31), np.real(magnetization).T)
 plt.set_cmap("RdYlBu")
 plt.colorbar()
 plt.title("Total Z-Magnetization Evolution")
 plt.xlabel("Site")
 plt.ylabel("time [ $Jt$ ]")
 
-plt.savefig("final_magnetisation_all0_withmpo.png")
+plt.savefig("mpo_final_magnetisation.png")
+
+# Single qubit evolving, compare with mps evolution
+# Single qubit rabii evolution
--- scratchpad/qtensor_MPS/test.py
+++ scratchpad/qtensor_MPS/test.py
@@ -260,6 +260,8 @@ def test_mpo_mps_inner_prod():
     mpo.construct_mpo(pauli_string)
 
     mps1 = MPS("q", n, 2)
+    # mps1.apply_single_qubit_gate(xgate(), 1)
+
     inner_prod1 = mpo.mpo_mps_inner_prod(mps1)
     condition1 = np.allclose(
         inner_prod1,
@@ -296,52 +298,14 @@ def test_mpo_gate_conjugate():
     mps = MPS("q", 3, 2)
 
     print(mpo.mpo_mps_inner_prod(mps))
-
-
-# def test_mpo_construction_from_two_qubit_gate_function():
-#     I = np.eye(2)
-#     CNOT = np.array(
-#         [
-#             [1.0, 0.0, 0.0, 0.0],
-#             [0.0, 1.0, 0.0, 0.0],
-#             [0.0, 0.0, 0.0, 1.0],
-#             [0.0, 0.0, 1.0, 0.0],
-#         ]
-#     )
-
-#     mps2 = MPS("q", 2, 2)
-#     mps2.apply_single_qubit_gate(xgate(), 0)
-#     mps2.apply_two_qubit_gate(cnot(), [0, 1])
-#     print("INITIAL MPS", mps2.get_wavefunction())
-
-#     gate_func = CNOT
-#     mpo = MPOLayer("q", 2, 2)
-#     mpo.construct_mpo(gate_func)
-#     mps = MPS("q", 2, 2)
-#     mps.apply_single_qubit_gate(xgate(), 0)
-#     mps.apply_mpo_layer(mpo)
-#     print("FINAL MPS", mps.get_wavefunction())
-
-#     condition = np.allclose(
-#         np.array(mps.get_wavefunction()),
-#         np.array(mps2.get_wavefunction()),
-#         rtol=1e-05,
-#         atol=1e-08,
-#     )
-#     message = "Arrays are not equal within tolerance"
-
-#     # assert condition, message
+    # Add assert statement
 
 
 # TODO:
-# Verify for two qubit gates. - (SVD and then apply it) (apply and then SVD)
-# Evolve half of circuit in mps and half in mpo.
-# Evaluate Bell state, epectation value of X at a particular index
-# Evaluate X, Y, Z observable
-# Check for one/two/three qubits (cover edge cases) for both mps/mpo
-# test_mpo_construction_from_pauli_string()
-# test_mpo_two_qubit_gate()
+
 # Initilisation given as pauli string and give mpo
 # Test for CNOT - Pauli then apply cnot + decompose cnot
 # expectation of an MPO - <psi| MPO | psi > psi = MPS (for this |psi | G1G2 X G2'G1' | psi>)
+
+# write vector of mps kronector of IZI ||
 test_mpo_gate_conjugate()
